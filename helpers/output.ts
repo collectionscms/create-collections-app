388ab45ad3fad8aabfc614f6cddc06d088944115
kazaneshimizu <ks@rocketa.co.jp>
2023-01-22 08:40:34 +0900

    chore: Log symbole add to console log.

--- helpers/output.ts
+++ helpers/output.ts
@@ -1,9 +1,10 @@
 import chalk from 'chalk';
+import logSymbols from 'log-symbols';
 
 const Output = {
-  success: (...output: any[]) => console.log(chalk.green('[Success]'), ...output),
-  error: (...output: any[]) => console.log(chalk.red('[Error]'), ...output),
-  info: (...output: any[]) => console.log(chalk.blue('[Info]'), ...output),
+  success: (...output: any[]) => console.log(`${logSymbols['success']} `, ...output),
+  error: (...output: any[]) => console.log(`${logSymbols['error']} `, ...output),
+  info: (...output: any[]) => console.log(chalk.blue(...output)),
   nextSteps: (projectName: string) => {
     console.log(`
 ${chalk.green('Awesome!')} You're now ready to start coding.
@@ -14,7 +15,7 @@ $ ${chalk.magentaBright(`cd ${projectName}`)}
 
 $ ${chalk.magentaBright('npm run dev')} - To start a local server for development.
 $ ${chalk.magentaBright('npm run build')} - To build a version for production.
-$ ${chalk.magentaBright('npm run start')} -To run the server in production.
+$ ${chalk.magentaBright('npm run start')} - To run the server in production.
 
 `);
   }
