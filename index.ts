bba1048af87dd68b71897ccf08ffdf5e0d3b6d29
kazaneshimizu <ks@rocketa.co.jp>
2023-01-22 08:42:20 +0900

    refactor: Template copy moved to init command.

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
-import { Argument, Command, Option } from 'commander';
-import packageJson from './package.json';
 import chalk from 'chalk';
-import { copyToProject, installProject } from './helpers/utils';
-import Output from "./helpers/output";
+import { Argument, Command } from 'commander';
+import execa from 'execa';
+import fse from 'fs-extra';
+import path from 'path';
+import Output from './helpers/output';
+import packageJson from './package.json';
 
 const program = new Command();
 
-program.version(packageJson.version, '-v, --version');
+program.version(packageJson.version, '-v, --version')
 program.usage(
   `${chalk.blue('project-directory')} [options]`
 );
@@ -19,10 +21,51 @@ program.addArgument(
 program.parse(process.argv);
 
 (async () => {
-  const projectDirectory = program.processedArgs[0];
+  const directory = program.processedArgs[0];
+  const rootPath = path.resolve(directory)
 
-  await copyToProject(projectDirectory)
-  await installProject(projectDirectory);
-  Output.nextSteps(projectDirectory);
+	if (await fse.pathExists(rootPath)) {
+		const stat = await fse.stat(rootPath);
+
+		if (stat.isDirectory() === false) {
+			Output.error(`Destination ${chalk.magentaBright(directory)} already exists and is not a directory.`)
+			process.exit(1);
+		}
+
+		const files = await fse.readdir(rootPath);
+
+		if (files.length > 0) {
+			Output.error(`Destination ${chalk.magentaBright(directory)} already exists and is not an empty directory.`)
+			process.exit(1);
+		}
+	}
+
+	Output.info(`Copying files to ${chalk.magentaBright(directory)} directory...`)
+
+	const onError = ({ err, exit = true }) => {
+		if (err) {
+			Output.error(err.stderr || err.stdout || 'Unknown error')
+		}
+		if (exit) {
+			process.exit(1);
+		}
+	};
+
+	try {
+		await execa('npx', ['superfastcms', 'init', directory]);
+	} catch (err) {
+		onError({err});
+	}
+
+	Output.info('Installing dependencies...')
+
+	try {
+		await execa('npm', ['install'], {cwd: directory, stdio: 'inherit'});
+	} catch (err) {
+		onError({err});
+	}
+
+	Output.success(`Successfully installed dependencies for ${chalk.magentaBright(directory)}.`)
+	Output.nextSteps(directory)
 })();
 
