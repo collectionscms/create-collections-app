d23e2a0b893d1b03860d72830aab908d9fed326b
kazaneshimizu <ks@rocketa.co.jp>
2023-07-30 07:40:20 +0900

    chore: formatted

--- helpers/output.ts
+++ helpers/output.ts
@@ -1,4 +1,4 @@
-import chalk from 'chalk';
+import chalk from "chalk";
 
 export const Output = {
   info: (...output: any[]) => console.log(`✨ `, ...output),
@@ -7,14 +7,14 @@ export const Output = {
   nextSteps: (projectName: string) => {
     console.log(`✅ You're now ready to start, so your next steps are`);
 
-    let message = chalk.green('NextSteps')
-    message += `\n\ncd ${projectName}`
-    message += `\n\n${chalk.grey('To run in development')}`
-    message += `\n\n${'npm run dev'}`
-    message += `\n\n${chalk.grey('To run in production')}`
-    message += `\n\n${'npm run build'}`
-    message += `\n${'npm run start'}`
+    let message = chalk.green("NextSteps");
+    message += `\n\ncd ${projectName}`;
+    message += `\n\n${chalk.grey("To run in development")}`;
+    message += `\n\n${"npm run dev"}`;
+    message += `\n\n${chalk.grey("To run in production")}`;
+    message += `\n\n${"npm run build"}`;
+    message += `\n${"npm run start"}`;
 
-    return message
-  }
+    return message;
+  },
 };
--- index.ts
+++ index.ts
@@ -1,62 +1,74 @@
 #!/usr/bin/env node
-import boxen from 'boxen';
-import chalk from 'chalk';
-import { Argument, Command } from 'commander';
-import { execa } from 'execa';
-import fse from 'fs-extra';
-import path from 'path';
-import { Output } from './helpers/output.js';
+import boxen from "boxen";
+import chalk from "chalk";
+import { Argument, Command } from "commander";
+import { execa } from "execa";
+import fse from "fs-extra";
+import path from "path";
+import { Output } from "./helpers/output.js";
 
 const program = new Command();
 
-program.version('0.2.0', '-v, --version')
-program.usage(
-  `${chalk.blue('project-directory')} [options]`
-);
+program.version("0.2.0", "-v, --version");
+program.usage(`${chalk.blue("project-directory")} [options]`);
 
 // Arguments
 program.addArgument(
-  new Argument('<project-directory>', 'specify path for create-superfast-app').argOptional().default('create-superfast-app')
+  new Argument("<project-directory>", "specify path for create-superfast-app")
+    .argOptional()
+    .default("create-superfast-app")
 );
 
 program.parse(process.argv);
 
 (async () => {
   const directory = program.processedArgs[0];
-  const rootPath = path.resolve(directory)
+  const rootPath = path.resolve(directory);
 
-	if (await fse.pathExists(rootPath)) {
-		const stat = await fse.stat(rootPath);
+  if (await fse.pathExists(rootPath)) {
+    const stat = await fse.stat(rootPath);
 
-		if (stat.isDirectory() === false) {
-			Output.error(`Destination ${chalk.magentaBright(directory)} already exists and is not a directory.`)
-			process.exit(1);
-		}
+    if (stat.isDirectory() === false) {
+      Output.error(
+        `Destination ${chalk.magentaBright(
+          directory
+        )} already exists and is not a directory.`
+      );
+      process.exit(1);
+    }
 
-		const files = await fse.readdir(rootPath);
+    const files = await fse.readdir(rootPath);
 
-		if (files.length > 0) {
-			Output.error(`Destination ${chalk.magentaBright(directory)} already exists and is not an empty directory.`)
-			process.exit(1);
-		}
-	} else {
-		await fse.mkdir(rootPath);
-	}
+    if (files.length > 0) {
+      Output.error(
+        `Destination ${chalk.magentaBright(
+          directory
+        )} already exists and is not an empty directory.`
+      );
+      process.exit(1);
+    }
+  } else {
+    await fse.mkdir(rootPath);
+  }
 
-	Output.info('Installing Superfast')
+  Output.info("Installing Superfast");
 
-	try {
-		await execa('npx', ['--silent', '-y', 'superfastcms', 'init', `-p ${directory}`], {stdio: 'inherit'});
-	} catch (err) {
-		Output.error(err.message || 'Unknown error')
-		process.exit(1);
-	}
+  try {
+    await execa(
+      "npx",
+      ["--silent", "-y", "superfastcms", "init", `-p ${directory}`],
+      { stdio: "inherit" }
+    );
+  } catch (err) {
+    Output.error(err.message || "Unknown error");
+    process.exit(1);
+  }
 
-	console.log(
-		boxen(Output.nextSteps(directory), {
-			padding: 1,
-			borderColor: 'green',
-			margin: 1,
-		})
-	);
+  console.log(
+    boxen(Output.nextSteps(directory), {
+      padding: 1,
+      borderColor: "green",
+      margin: 1,
+    })
+  );
 })();
